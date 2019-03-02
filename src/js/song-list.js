ebf6454da08c6521d3db020166aac581353adeac
Gene0 <445119186@qq.com>
2019-03-02 20:25:20 +0800

    选中歌曲后在表单里展示歌曲

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -10,16 +10,16 @@
             let $el = $(this.el)
             $el.html(this.template)
             let {songs} = data
-            let liList = songs.map((song)=> $('<li></li>').text(song.name).attr('data-song-id', song.id))
+            let liList = songs.map((song)=> $('<li></li>').text(song.name).attr('data-song-id',song.id))
             $el.find('ul').empty()
             liList.map((domLi)=>{
                 $el.find('ul').append(domLi)
             })
         },
         activeItem(li){
-            let $li = $(li)
+            let $li=$(li)
             $li.addClass('active')
-               .siblings('.active').removeClass('active')
+                .siblings('.active').removeClass('active')
         },
         clearActive(){
             $(this.el).find('.active').removeClass('active')
@@ -56,8 +56,17 @@
         bindEvents(){
             $(this.view.el).on('click', 'li', (e)=>{
                 this.view.activeItem(e.currentTarget)
-                let songId = e.currentTarget.getAttribute('data-song-id')
-                window.eventHub.emit('select', {id:songId})
+                let songId=e.currentTarget.getAttribute('data-song-id')
+                let data
+                let songs = this.model.data.songs
+                for(var i=0;i<songs.length;i++){
+                    if(songs[i].id === songId){
+                        data = songs[i] 
+                        break
+                    }
+                }
+                window.eventHub.emit('select', JSON.parse(JSON.stringify(data)))
+                
             })
         },
         bindEventHub(){
